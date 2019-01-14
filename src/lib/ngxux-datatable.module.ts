e7f825570a26e3656dd87783b88512dc4dd88655
Matthew Davis <matthew@matthewdavis.io>
2019-01-14 17:06:17 -0500

    ngx-datatable

--- src/lib/ngxux-datatable.module.ts
+++ src/lib/ngxux-datatable.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule }                from '@angular/core';
+import { NgxDatatableModule }      from '@swimlane/ngx-datatable';
 import { NgxuxDatatableComponent } from './ngxux-datatable.component';
 
 @NgModule({
-  declarations: [NgxuxDatatableComponent],
-  imports: [
+
+  declarations: [
+
+      NgxuxDatatableComponent
+
   ],
-  exports: [NgxuxDatatableComponent]
+
+  imports: [
+
+      NgxDatatableModule
+
+  ],
+
+  exports: [
+
+      NgxuxDatatableComponent
+
+  ]
+    
 })
 export class NgxuxDatatableModule { }
